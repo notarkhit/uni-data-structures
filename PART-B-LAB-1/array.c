c6e65d6fce8439f2bd5801e68f34187db9b3ab72
notarkhit <notarkhit@gmail.com>
2025-01-09 21:51:10 +0530

    add array.c

---
+++ PART-B-LAB-1/array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int array[20],size;
+
+void traverseArray( void );
+void insertElement( int, int );
+void deleteElement( int );
+
+int main() {
+
+	return 0;
+}
