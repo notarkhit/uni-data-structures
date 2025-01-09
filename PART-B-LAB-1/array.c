ca06450d1aa15b49284ff8ec87fb28e5382ea3bc
notarkhit <notarkhit@gmail.com>
2025-01-09 22:03:05 +0530

    update array.c

--- PART-B-LAB-1/array.c
+++ PART-B-LAB-1/array.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,6 +9,16 @@ void insertElement( int, int );
 void deleteElement( int );
 
 int main() {
+	
+	int position, element, i;
+	system("clear");
+	
+	printf("Enter the size of the array: ");
+	scanf("%d",&size);
+	system("clear");
+	printf("Array size set to : %d press any key to continue:  ");
+	getchar();
+	system("clear");
 
 	return 0;
 }
