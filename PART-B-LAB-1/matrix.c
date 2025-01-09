5331065cb24f1f81add4b6869a431488cb54057d
notarkhit <notarkhit@gmail.com>
2025-01-09 21:45:10 +0530

    remove test code

--- PART-B-LAB-1/matrix.c
+++ PART-B-LAB-1/matrix.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 
-char getch() {
-	char ch;
-	ch = getchar();
-    return ch;
-}
-
 int main() {
-
-	printf("hello");
-	getchar();
-	printf("world");
-	getchar();
 	return 0;
 }
