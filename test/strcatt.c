5c2b4758b42ddc683667e9ed19e6baa540ffa97d
notarkhit <notarkhit@gmail.com>
2025-01-29 09:13:58 +0530

    updates

--- test/strcatt.c
+++ test/strcatt.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 void strcatt(char[],char[]);
 
 int main(){
 
 	int stringSize = 20;
-	char firstString[stringSize], secondString[stringSize];
+	char firstString[stringSize], secondString[stringSize], catString[stringSize*2];
 
 	printf("Enter First String: ");
 	fgets(firstString, stringSize, stdin );
 	printf("Enter second String: ");
 	fgets(secondString, stringSize, stdin );
 
+	strcatt(firstString, secondString);
 
-	printf("%s%s", firstString, secondString);
-
+	printf("concatenated string: %s",catString);
 
 	return 0;
 
+}
+
+void strcatt(char s1[], char s2[]){
+	int i;
 }
